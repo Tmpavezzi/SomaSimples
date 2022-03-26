cd4be0efc275dd322a697f9f64d7be0d612d9f85
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-03-25 21:51:19 -0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,17 @@
+exer 01
+
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    int n1 , n2 ;
+    System.out.print (" Escreva um numero " );
+    n1  = scanner .nextInt();
+    System.out.print ("Escreva um Numero ");
+    n2 = scanner .nextInt();
+    int Soma = n1 * n2 ;
+    System.out.println ("Resutado "  +  Soma);
+    
+  }
+}
